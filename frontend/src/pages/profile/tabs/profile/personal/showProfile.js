a9fd61f51324778fae00031b0cb0197c2fecb29c
Merve Bingol <mervebingol@gmail.com>
2023-12-08 11:11:28 +0300

    fix:showProfile

--- frontend/src/pages/profile/tabs/profile/personal/showProfile.js
+++ frontend/src/pages/profile/tabs/profile/personal/showProfile.js
@@ -2,41 +2,135 @@ import api from "../../../../../services/api";
 import { useState, useEffect } from "react";
 import Select from "react-select";
 import Table from "../../../../../components/layout/table/table";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp, faPlus } from "@fortawesome/free-solid-svg-icons";
 import "../../../../../styles/styles.scss"
 import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 
 export default function Profile(props) {
+  const location = useLocation();
+  const { userId } = location.state;
+
+  const languages = [
+    { label: "Turkish", value: "Turkish" },
+    { label: "English", value: "English" },
+    { label: "Russian", value: "Russian" },
+    { label: "Spanish", value: "Spanish" },
+    { label: "Chinese", value: "Chinese" },
+    { label: "Arabic", value: "Arabic" },
+  ];
+
+  const skills = [
+    { label: "Angular", value: "angular" },
+    { label: "CSS", value: "css" },
+    { label: "Graphic Design", value: "design" },
+    { label: "Ember", value: "ember" },
+    { label: "HTML", value: "html" },
+    { label: "Information Architecture", value: "ia" },
+    { label: "Javascript", value: "javascript" },
+    { label: "Mechanical Engineering", value: "mech" },
+    { label: "Meteor", value: "meteor" },
+    { label: "NodeJS", value: "node" },
+    { label: "Plumbing", value: "plumbing" },
+    { label: "Python", value: "python" },
+    { label: "Rails", value: "rails" },
+    { label: "React", value: "react" },
+    { label: "Kitchen Repair", value: "repair" },
+    { label: "Ruby", value: "ruby" },
+    { label: "UI Design", value: "ui" },
+    { label: "User Experience", value: "ux" },
+  ];
+
   const educationHeadlines = ["School", "Section", "Graduation Date"];
   const projectHeadlines = ["Project Name", "Description", "Date"];
   const achievementHeadlines = ["Name", "Description"];
   const experienceHeadlines = ["Name", "Company", "Description", "Date"];
 
+  const genderOptions = [
+    {
+      label: "Male",
+      value: "1",
+    },
+    {
+      label: "Female",
+      value: "2",
+    },
+  ];
+
+  const profileData = props.profile;
+
+  //profile data dbden geliyor formu dolduruyor
 
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState(profileData);
 
-  const [languagesOptions, setLanguagesOptions] = useState([]);
-
-
-
-  const getProfile = async () => {
-    try {
-      const res = await api.user.profile.get("65590d6087e8c71e81e39160");
-      setProfile(res);
-      console.log("Alınan Profil:", res);
-    } catch (error) {
-      console.error("Profil verileri alınırken hata oluştu:", error);
-    }
-  };
 
  
+
+  const [languagesOptions, setLanguagesOptions] = useState([]);
+  const [selectedLanguages, setSelectedLanguages] = useState([]); 
+
+  const [skillsOptions, setSkillsOptions] = useState([]);
+  const [selectedSkills, setSelectedSkills] = useState([]);
+
+  
+
+  const { id } = useParams();
+
+
+  const getProfile1 = async () => {
+   
+    try {
+
+
+      const res = await api.user.profile.get(userId)
+
+    
+        setForm(res)
+
+        console.log("bu da res:",res)
+
+    } catch (error) {
+
+    }
+    
+  };
+
   useEffect(() => {
-    getProfile();
+    getProfile1();
   }, []);
 
+ 
+
+  const formattedEducations = form?.educations.map((education) => ({
+    id: education._id,
+    school: education.school,
+    section: education.section,
+    date: education.date,
+  }));
+
+  const formattedProjects = form?.projects.map((project)=>({
+    id:project._id,
+    headline:project.headline,
+    description:project.description,
+    date:project.date
+  }))
+  const formattedAchievements = form?.achievements.map((achievement)=>({
+    id:achievement._id,
+    headline:achievement.headline,
+    description:achievement.description,
+  }))
+  const formattedExperiences = form?.experiences.map((experience)=>({
+    id:experience._id,
+    headline:experience.headline,
+    company: experience.company,
+    description:experience.description,
+    date:experience.date
+  }))
 
   return (
     <>
@@ -53,7 +147,7 @@ export default function Profile(props) {
               <label class="">First Name</label>
               <input
                 type="input"
-                value={profile?.firstName}
+                value={form?.firstName}
                 required
                 readOnly
          
@@ -65,7 +159,7 @@ export default function Profile(props) {
                 type="input"
                 required
                 readOnly
-                value={profile?.lastName}
+                value={form?.lastName}
            
               />
             </li>
@@ -75,14 +169,17 @@ export default function Profile(props) {
                 type="date"
                 required
                 readOnly
-                defaultValue={profile?.birthday}
+                defaultValue={form?.birthday}
             
               />
             </li>
             <li>
               <label>Gender</label>
               <Select
-                value={profile?.gender}
+                value={form?.gender}
+                options={genderOptions}
+                getOptionLabel={(x) => x.label}
+                getOptionValue={(x) => x.value}
                 unstyled
                 readOnly
                 className="react-select-container"
@@ -93,7 +190,7 @@ export default function Profile(props) {
             <li>
               <label>Languages</label>
               <Select
-                value={profile?.languages}
+                value={selectedLanguages}
                 options={languagesOptions}
                 isMulti
                 unstyled
@@ -106,7 +203,8 @@ export default function Profile(props) {
             <li>
               <label>Skills</label>
               <Select
-                 value={profile?.skills}
+                 value={selectedSkills}
+                 options={skillsOptions}
                  isMulti
                  unstyled
                  readOnly
@@ -120,14 +218,14 @@ export default function Profile(props) {
               <textarea
                 required
                 readOnly
-                value={profile?.profileDescription}
+                value={form?.profileDescription}
                
               />
             </li>
             <li>
               <label>Address</label>
               <textarea
-                value={profile?.address}
+                value={form?.address}
                 required
                 readOnly
                
@@ -140,7 +238,7 @@ export default function Profile(props) {
           <div className="card__header">Education Information</div>
           <ul className="card__body">
           <Table
-              data={profile?.educations}
+              data={formattedEducations}
               headline={educationHeadlines}
              
               loading={loading}
@@ -152,7 +250,7 @@ export default function Profile(props) {
           <div className="card__header">Projects</div>
           <ul className="card__body">
             <Table
-              data={profile?.projects}
+              data={formattedProjects}
               headline={projectHeadlines}
              
               loading={loading}
@@ -165,27 +263,34 @@ export default function Profile(props) {
           <div className="card__header">Achievement</div>
           <ul className="card__body">
             <Table
-              data={profile?.achievements}
+              data={formattedAchievements}
               headline={achievementHeadlines}
            
               loading={loading}
             />
+
+          
           </ul>
         </div>
         <div className="card">
           <div className="card__header">Experiences</div>
           <ul className="card__body">
             <Table
-              data={profile?.experiences}
+              data={formattedExperiences}
               headline={experienceHeadlines}
+           
               loading={loading}
-            />      
+            />
+
+          
           </ul>
         </div>
+
+
+
       </div>
       </div>
       </div>
     </>
   );
 }
-
